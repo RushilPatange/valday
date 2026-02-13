7c79b6af63f449495fb9c0cfe2a2bdd0fc2051af
Rushil Patange <rushilpatange5@gmail.com>
2026-02-13 17:20:59 -0600

    fix github pages routing

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/valday/", // <--- THIS IS THE MAGIC LINE
   server: {
     host: "::",
     port: 8080,
